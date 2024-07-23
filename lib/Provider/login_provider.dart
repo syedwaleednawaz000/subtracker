ffda3595a21360b5717281b4d666aaffbf4318f3
syedwaleednawaz000 <syedwaleednawaz333@gmail.com>
2024-07-23 16:06:30 +0500

    by waleed set ogin and regiter provder

--- lib/Provider/login_provider.dart
+++ lib/Provider/login_provider.dart
@@ -1,11 +1,12 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sub_tracker/Repo/repo.dart';
 import 'package:sub_tracker/utils/flutter_toast.dart';
 
 class LoginProvider extends ChangeNotifier{
-  ApiService _apiService = ApiService();
+  final ApiService _apiService = ApiService();
 
   bool isLogin = false;
   void _loginLoading({required bool load}){
@@ -13,15 +14,18 @@ class LoginProvider extends ChangeNotifier{
     notifyListeners();
   }
 
-  Future<void> login()async{
+  Future<void> login({required String email , required String password, required bool rememberMe})async{
     FlutterToast.toastMessage(message: "Login api call ");
     var body = {
-      "email": "wedfghj",
-      "password": "azxscdfvgbn"
+      "email": email,
+      "password": password
     };
     try{
       Response response = await _apiService.login(params: body);
       if(response.statusCode == 200){
+        if(rememberMe == true){
+          storeRememberMe(email: email,password: password);
+        }
         if (kDebugMode) {
           print("hit successfully");
         }
@@ -34,6 +38,27 @@ class LoginProvider extends ChangeNotifier{
     }catch(error){
       print("this is error ${error.toString()}");
     }
+  }
+
+
+  Future<void> storeLocalData()async{
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+  }
+  Future<void> getLocalData()async{
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+  }
+  Future<void> cleanLocalData()async{
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+  }
+  Future<void> storeRememberMe({required String email, required String password })async{
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString("", email);
+    prefs.setString("", password);
+  }
+  Future<void> getRememberMe()async{
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.getString('');
+    prefs.getString('');
   }
 
 }
