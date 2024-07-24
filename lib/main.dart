bf7c5b715cafc56b93d6e06f431309f37258798f
Hamza-Asghar <asgharhamza2003@gmail.com>
2024-07-24 12:34:37 +0500

    by waleed

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:sub_tracker/bottom_nav/bottom_navBar.dart';
-import 'package:sub_tracker/test.dart';
 import 'package:sub_tracker/theme/theme.dart';
 import 'package:sub_tracker/utils/my_size.dart';
-import 'package:sub_tracker/views/auth/login/login_screen.dart';
-import 'package:sub_tracker/views/auth/signUp/sigup_screen.dart';
-import 'package:sub_tracker/views/bottomnavbar/bottomnavbar.dart';
-import 'package:sub_tracker/views/cancelsubscription/cancelsubscription.dart';
-import 'package:sub_tracker/views/contactsupport/contactsupport.dart';
 import 'package:sub_tracker/views/forgot_password/base/countNotifier.dart';
-import 'package:sub_tracker/views/forgot_password/forget_password.dart';
-import 'package:sub_tracker/views/home_screen/home_screen.dart';
-import 'package:sub_tracker/views/language_selection/language_selection.dart';
-import 'package:sub_tracker/views/manageplan/manageplan.dart';
-import 'package:sub_tracker/views/payment_method/payment_screen.dart';
-import 'package:sub_tracker/views/settings/settings.dart';
-import 'package:sub_tracker/views/spending_budgets/spending_budgets.dart';
 import 'package:sub_tracker/views/splash_screen/splash_screen.dart';
-import 'package:sub_tracker/views/subscription/subscription.dart';
-import 'package:sub_tracker/views/subscriptioninfo/subscription_info.dart';
-import 'package:sub_tracker/views/welcomeScreen/welcome_screen.dart';
 import 'bottom_nav/bottomNotifier.dart';
 import 'package:provider/provider.dart';
 
