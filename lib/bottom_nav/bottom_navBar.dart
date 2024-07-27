d6be9826a14f6e00eab248394e27e691058583f7
muhammad-faisal749 <mrfaisal749@gmail.com>
2024-07-27 15:39:16 +0500

    faisal pushed

--- lib/bottom_nav/bottom_navBar.dart
+++ lib/bottom_nav/bottom_navBar.dart
@@ -121,7 +121,6 @@ import 'bottomNotifier.dart';
 //   }
 // }
 
-
 class BnavBar extends StatefulWidget {
   const BnavBar({super.key});
 
@@ -136,74 +135,100 @@ class _BnavBarState extends State<BnavBar> {
     const CalendarScreen(),
     const Settings(),
   ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
         floatingActionButton: CircleAvatar(
           radius: 26,
           backgroundColor: Colors.white,
           child: FloatingActionButton(
             backgroundColor: Colors.blue,
-              shape: CircleBorder(),
-              onPressed: (){},
-            child: Icon(Icons.add, ),
+            shape: CircleBorder(),
+            onPressed: () {},
+            child: Icon(
+              Icons.add,
+            ),
           ),
         ),
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.all(18.0),
-        child: BottomNavigationBar(
-            type: BottomNavigationBarType.fixed,
-            backgroundColor: Color(0XFF4E4E61),
-            showSelectedLabels: false,
-            showUnselectedLabels: false,
-            items: [
-              BottomNavigationBarItem(
-                  icon:   Consumer<BottomNotifier>(builder: (context, value, child) {
-                    print('consumer tapped');
-                    return  IconButton(
-                      icon: Image.asset(AppImages.homeIcon, width: 18, height: 18, color: value.isSelected(0) ? Color(0XFF758AFF) : Color(0XFFC1C1CD)),
-                      onPressed: () => value.selection(0),
-                      // color: value.selectedIndex == 1 ? Colors.blue : Colors.grey,
-
-                    );
-                  },), label: ''),
-              BottomNavigationBarItem(
-                  icon:  Consumer<BottomNotifier>(builder: (context, value, child) {
-                    return  IconButton(
-                      icon: Image.asset(AppImages.dashboardIcon, width: 18, height: 18,
-                          // color: value.selectedColor ? Color(0XFF758AFF) :  Color(0XFFC1C1CD),
-                          color: value.isSelected(1) ?Color(0XFF758AFF) : Color(0XFFC1C1CD)
-                      ),
-                      onPressed: () => value.selection(1),
-                      // color: value.selectedIndex == 2 ? Colors.blue : Colors.grey,
-                    );
-                  }),label: ''),
-              BottomNavigationBarItem(
-                  icon:   Consumer<BottomNotifier>(builder: (context, value, index) {
-                    return  IconButton(
-                      icon: Image.asset(AppImages.calendarIcon, width: 18, height: 18,
-                          // color: value.selectedColor ? Color(0XFF758AFF) :  Color(0XFFC1C1CD),
-                          color: value.isSelected(2) ? Color(0XFF758AFF) : Color(0XFFC1C1CD)
-                      ),
-                      onPressed: () => value.selection(2),
-                      // color: value.selectedIndex == 2 ? Colors.blue : Colors.grey,
-                    );
-                  }), label: ''),
-              BottomNavigationBarItem(
-            icon:   Consumer<BottomNotifier>(builder: (context, value, child) {
-                 return  IconButton(
-                   icon: Icon(Icons.settings, size: 22,   color: value.isSelected(3) ? Color(0XFF758AFF) : Color(0XFFC1C1CD)),
-                   // icon: Image.asset(AppImages.setting, width: 18, height: 18, color: AppColors.white100,),
-                   onPressed: () => value.selection(3),
-                   // color: value.selectedIndex == 3 ? Colors.blue : Colors.grey,
-
-                 );
-               },), label: ''),
-        ]),
-      ),
-        body: Consumer<BottomNotifier>(builder: (context, value, child){
-    return _screens[value.selectedIndex];
-    }));
+        bottomNavigationBar: Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: BottomNavigationBar(
+              type: BottomNavigationBarType.fixed,
+              backgroundColor: Color(0XFF4E4E61),
+              showSelectedLabels: false,
+              showUnselectedLabels: false,
+              items: [
+                BottomNavigationBarItem(
+                    icon: Consumer<BottomNotifier>(
+                      builder: (context, value, child) {
+                        print('consumer tapped');
+                        return IconButton(
+                          icon: Image.asset(AppImages.homeIcon,
+                              width: 18,
+                              height: 18,
+                              color: value.isSelected(0)
+                                  ? Color(0XFF758AFF)
+                                  : Color(0XFFC1C1CD)),
+                          onPressed: () => value.selection(0),
+                          // color: value.selectedIndex == 1 ? Colors.blue : Colors.grey,
+                        );
+                      },
+                    ),
+                    label: ''),
+                BottomNavigationBarItem(
+                    icon: Consumer<BottomNotifier>(
+                        builder: (context, value, child) {
+                      return IconButton(
+                        icon: Image.asset(AppImages.dashboardIcon,
+                            width: 18,
+                            height: 18,
+                            // color: value.selectedColor ? Color(0XFF758AFF) :  Color(0XFFC1C1CD),
+                            color: value.isSelected(1)
+                                ? Color(0XFF758AFF)
+                                : Color(0XFFC1C1CD)),
+                        onPressed: () => value.selection(1),
+                        // color: value.selectedIndex == 2 ? Colors.blue : Colors.grey,
+                      );
+                    }),
+                    label: ''),
+                BottomNavigationBarItem(
+                    icon: Consumer<BottomNotifier>(
+                        builder: (context, value, index) {
+                      return IconButton(
+                        icon: Image.asset(AppImages.calendarIcon,
+                            width: 18,
+                            height: 18,
+                            // color: value.selectedColor ? Color(0XFF758AFF) :  Color(0XFFC1C1CD),
+                            color: value.isSelected(2)
+                                ? Color(0XFF758AFF)
+                                : Color(0XFFC1C1CD)),
+                        onPressed: () => value.selection(2),
+                        // color: value.selectedIndex == 2 ? Colors.blue : Colors.grey,
+                      );
+                    }),
+                    label: ''),
+                BottomNavigationBarItem(
+                    icon: Consumer<BottomNotifier>(
+                      builder: (context, value, child) {
+                        return IconButton(
+                          icon: Icon(Icons.settings,
+                              size: 22,
+                              color: value.isSelected(3)
+                                  ? Color(0XFF758AFF)
+                                  : Color(0XFFC1C1CD)),
+                          // icon: Image.asset(AppImages.setting, width: 18, height: 18, color: AppColors.white100,),
+                          onPressed: () => value.selection(3),
+                          // color: value.selectedIndex == 3 ? Colors.blue : Colors.grey,
+                        );
+                      },
+                    ),
+                    label: ''),
+              ]),
+        ),
+        body: Consumer<BottomNotifier>(builder: (context, value, child) {
+          return _screens[value.selectedIndex];
+        }));
   }
 }
